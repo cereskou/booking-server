38223a129b2772836ec4cd486a63fe981213ac30
cereskou <cereskou@gmail.com>
2020-12-14 09:01:06 +0900

    Casbin middleware

---
+++ casbin/mw.go
@@ -0,0 +1,106 @@
+package mw
+
+import (
+	"ditto/booking/security"
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/casbin/casbin/v2"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+//CasbinConfig -
+type CasbinConfig struct {
+	// Skipper defines a function to skip middleware.
+	Skipper middleware.Skipper
+
+	// Enforcer CasbinAuth main rule.
+	// Required.
+	Enforcer *casbin.Enforcer
+}
+
+var (
+	// DefaultConfig is the default CasbinAuth middleware config.
+	DefaultConfig = CasbinConfig{
+		Skipper: middleware.DefaultSkipper,
+	}
+)
+
+// Middleware returns a CasbinAuth middleware.
+//
+// For valid credentials it calls the next handler.
+// For missing or invalid credentials, it sends "401 - Unauthorized" response.
+func Middleware(ce *casbin.Enforcer) echo.MiddlewareFunc {
+	c := DefaultConfig
+	c.Enforcer = ce
+	return MiddlewareWithConfig(c)
+}
+
+// MiddlewareWithConfig returns a CasbinAuth middleware with config.
+// See `Middleware()`.
+func MiddlewareWithConfig(config CasbinConfig) echo.MiddlewareFunc {
+	// Defaults
+	if config.Skipper == nil {
+		config.Skipper = DefaultConfig.Skipper
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if config.Skipper(c) {
+				return next(c)
+			}
+
+			if pass, err := config.CheckPermission(c); err == nil && pass {
+				return next(c)
+			} else if err != nil {
+				return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+			}
+
+			return echo.ErrForbidden
+		}
+	}
+}
+
+// CheckPermission checks the user/method/path combination from the request.
+// Returns true (permission granted) or false (permission forbidden)
+func (a *CasbinConfig) CheckPermission(c echo.Context) (bool, error) {
+	role := "anonymous"
+	user := c.Get("user")
+	if user != nil {
+		role = roleFromToken(c)
+	}
+	method := c.Request().Method
+	path := c.Request().URL.Path
+
+	//複数対応
+	r := strings.Split(role, ",")
+	if len(r) > 1 {
+		for _, r0 := range r {
+			b, _ := a.Enforcer.Enforce(r0, path, method)
+			if b {
+				return b, nil
+			}
+		}
+		return false, errors.New("Forbidden")
+	}
+
+	return a.Enforcer.Enforce(role, path, method)
+}
+
+//decode role from token
+func roleFromToken(c echo.Context) string {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+
+	secret := claims["name"].(string)
+	payload := security.DecryptString(secret)
+	bodys := strings.Split(payload, "|")
+	if len(bodys) == 3 {
+		return bodys[2]
+	}
+
+	return ""
+}
