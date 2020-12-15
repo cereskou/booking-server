9a8362fe5062f298801996907cbf9b3f43d21a43
cereskou <cereskou@gmail.com>
2020-12-15 21:56:40 +0900

    Add EncryptSlice

--- security/encode.go
+++ security/encode.go
@@ -13,6 +13,31 @@ import (
 // TODO - this password is for development. please change it first before build for production.
 var PASSWORD []byte = []byte("YcsEW=jN83%!=Ym&@86sX3hBBuUT486X")
 
+//EncryptSlice -
+func EncryptSlice(plaintext []byte) (encryptedString string) {
+	//Create a new Cipher Block from the key
+	block, err := aes.NewCipher(PASSWORD)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//Create a new GCM
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//Create a nonce. Nonce should be from GCM
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+
+	//Encrypt the data using aesGCM.Seal
+	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
+	return fmt.Sprintf("%x", ciphertext)
+}
+
 //EncryptString -
 func EncryptString(stringToEncrypt string) (encryptedString string) {
 	//Since the key is in string, we need to convert decode it to bytes
