2bb71e8099da52767ac7f449ac5c7d8ca89831e9
cereskou <cereskou@gmail.com>
2020-12-15 21:54:00 +0900

    Change /member to /user

--- api/routes.go
+++ api/routes.go
@@ -16,10 +16,24 @@ func (s *Service) RegisterRoutes(e *echo.Echo, prefix string) {
 	}
 	g := e.Group(prefix)
 
-	g.POST("/login", s.Login)
-
+	//anonymous
+	//load
+	g.POST("/user/login", s.Login)
 	//refresh token
-	g.POST("/refresh", s.Token)
+	g.POST("/user/refresh", s.RefreshToken)
+
+	//User
+	u := g.Group("/user")
+	u.Use(middleware.JWTWithConfig(config))
+	u.Use(casbinmw.Middleware(s._enforcer))
+	//get user detail
+	u.GET("", s.GetUser)
+	//get login detail
+	u.GET("/account", s.GetAccount)
+	//update user
+	u.PUT("", s.UpdateUser)
+	//update user password
+	u.PUT("/password", s.UpdatePassword)
 
 	//Holiday
 	g.GET("/holidays/:year", s.ListHolidays)
@@ -28,15 +42,11 @@ func (s *Service) RegisterRoutes(e *echo.Echo, prefix string) {
 	h.Use(casbinmw.Middleware(s._enforcer))
 	h.POST("", s.UpdateHolidays)
 
-	u := g.Group("/member")
-	u.Use(middleware.JWTWithConfig(config))
-	u.Use(casbinmw.Middleware(s._enforcer))
-	//User
-	u.GET("/detail", s.User)
-
-	//
+	//Admin
 	a := g.Group("/admin")
 	a.Use(middleware.JWTWithConfig(config))
 	a.Use(casbinmw.Middleware(s._enforcer))
-	a.GET("/member/:email", s.Member)
+	a.GET("/user/:email", s.AdminGetUser)
+	a.GET("/account/:email", s.AdminGetAccount)
+	a.PUT("/user/:email", s.AdminUpdateUser)
 }
