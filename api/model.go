fa98c6daead00e97d57ee05052977a914245d0fb
cereskou <cereskou@gmail.com>
2020-12-15 21:53:36 +0900

    Update model.go

--- api/model.go
+++ api/model.go
@@ -1,10 +1,14 @@
 package api
 
+import (
+	"time"
+)
+
 //Response -
 type Response struct {
 	Code  int         `json:"code"`
-	Error string      `json:"error"`
-	Data  interface{} `json:"data"`
+	Error string      `json:"error,omitempty"`
+	Data  interface{} `json:"data,omitempty"`
 }
 
 // HTTPError represents an error that occurred while handling a request.
@@ -14,6 +18,15 @@ type HTTPError struct {
 	Message  interface{} `json:"message"`
 	Internal error       `json:"-"` // Stores the error returned by an external dependency
 }
+
+//KeyValue -
+type KeyValue struct {
+	Key   string      `json:"key"`
+	Value interface{} `json:"value"`
+}
+
+//Empty -
+type Empty struct{}
 
 //Login -
 type Login struct {
@@ -25,4 +38,27 @@ type Login struct {
 type RefreshToken struct {
 	GrantType string `json:"grant_type"`
 	Token     string `json:"refresh_token"`
+}
+
+//Payload - access token payload
+type Payload struct {
+	ID    int64  `json:"id"`
+	Email string `json:"email"`
+	Name  string `json:"name"`
+	Role  string `json:"role"`
+}
+
+//User - update user password
+type User struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+//Password - update user password
+type Password struct {
+	Email       string    `json:"email"`        //Email
+	NewPassword string    `json:"new_password"` //New Password
+	OldPassword string    `json:"old_password"` //Old Password
+	UpdateDate  time.Time `json:"update_date"`  //更新日時
 }
