8d4cdf951ec4d38470b881d2624369fa256dd0e6
cereskou <cereskou@gmail.com>
2020-12-15 21:50:49 +0900

    Add StructToJSONTagMap

--- utils/json.go
+++ utils/json.go
@@ -1,8 +1,52 @@
 package utils
 
-import jsoniter "github.com/json-iterator/go"
+import (
+	"reflect"
+
+	jsoniter "github.com/json-iterator/go"
+)
 
 //JSON -
 var (
 	JSON = jsoniter.ConfigCompatibleWithStandardLibrary
 )
+
+//StructToJSONTagMap -
+func StructToJSONTagMap(data interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	elem := reflect.ValueOf(data).Elem()
+	size := elem.NumField()
+
+	for i := 0; i < size; i++ {
+		field := elem.Type().Field(i).Tag.Get("json")
+		value := elem.Field(i).Interface()
+		result[field] = value
+	}
+
+	return result
+}
+
+//StructToMap -
+func StructToMap(data interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	elem := reflect.ValueOf(data).Elem()
+	size := elem.NumField()
+
+	for i := 0; i < size; i++ {
+		field := elem.Type().Field(i).Name
+		value := elem.Field(i).Interface()
+		result[field] = value
+	}
+
+	return result
+}
+
+//UnmarshalStruct -
+func UnmarshalStruct(data interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+
+	b, _ := JSON.Marshal(data)
+	JSON.Unmarshal(b, &result)
+
+	return result
+}
