1e9be57094e11420887cc8756aafeb9480f969e2
cereskou <cereskou@gmail.com>
2020-12-20 00:16:54 +0900

    Update comm.go

--- cx/comm.go
+++ cx/comm.go
@@ -2,9 +2,8 @@ package cx
 
 //Payload - access token payload
 type Payload struct {
-	ID     int64  `json:"id"`     //ユーザーID
-	Email  string `json:"email"`  //メールアドレス
-	Name   string `json:"name"`   //名前
-	Role   string `json:"role"`   //ロール
-	Tenant int64  `json:"tenant"` //所属コンテナ
+	ID     int64  `json:"id"`    //ユーザーID
+	Email  string `json:"email"` //メールアドレス
+	Role   string `json:"role"`  //ロール
+	Tenant int64  `json:"-"`     //テナントID
 }
