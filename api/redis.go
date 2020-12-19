20a9f8458a6e16829d64be900f2313169e2dc7b3
cereskou <cereskou@gmail.com>
2020-12-20 00:15:57 +0900

    Update redis.go

--- api/redis.go
+++ api/redis.go
@@ -16,6 +16,15 @@ func (s *Service) HGetAll(key string) (map[string]string, error) {
 	return nil, errors.New("Not connect REDIS")
 }
 
+//HGet -
+func (s *Service) HGet(key, field string) (string, error) {
+	client := s.Redis()
+	if client != nil {
+		return client.HGet(key, field).Result()
+	}
+	return "", errors.New("Not connect REDIS")
+}
+
 //HSet -
 func (s *Service) HSet(key, field string, value interface{}) error {
 	client := s.Redis()
@@ -69,6 +78,40 @@ func (s *Service) CacheDel(key string) error {
 	client := s.Redis()
 	if client != nil {
 		return client.Del(key).Err()
+	}
+
+	return errors.New("Not connect REDIS")
+}
+
+//CacheMultiDel -
+func (s *Service) CacheMultiDel(pattern string) error {
+	client := s.Redis()
+	if client != nil {
+		var cursor uint64
+		var err error
+		keys := make([]string, 0)
+		for {
+			var k []string
+			if k, cursor, err = client.Scan(cursor, pattern, 100).Result(); err != nil {
+				break
+			}
+			keys = append(keys, k...)
+
+			if cursor == 0 {
+				break
+			}
+		}
+
+		pipe := client.Pipeline()
+		for _, k := range keys {
+			pipe.Del(k)
+		}
+		_, err = pipe.Exec()
+		if err != nil {
+			return err
+		}
+
+		return nil
 	}
 
 	return errors.New("Not connect REDIS")
