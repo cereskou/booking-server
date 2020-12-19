483d4672014bfbc73510e25eb1dd17a5f11aa20e
cereskou <cereskou@gmail.com>
2020-12-20 00:12:53 +0900

    エラー処理やり直し

--- api/auth.go
+++ api/auth.go
@@ -2,67 +2,61 @@ package api
 
 import (
 	"ditto/booking/cx"
-	"ditto/booking/security"
 	"ditto/booking/utils"
-	"net/http"
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo/v4"
 )
 
-//Unauthorized -
-func Unauthorized() *Response {
-	return &Response{
-		Code: http.StatusUnauthorized,
-		Data: "Invalid Username or Password",
-	}
-}
-
-//BadRequest -
-func BadRequest(err error) *Response {
-	return &Response{
-		Code:  http.StatusBadRequest,
-		Error: err.Error(),
-	}
-}
-
-//NotFound -
-func NotFound(err error) *Response {
-	return &Response{
-		Code:  http.StatusNotFound,
-		Error: err.Error(),
-	}
-}
-
-//InternalServerError -
-func InternalServerError(err error) *Response {
-	return &Response{
-		Code:  http.StatusInternalServerError,
-		Error: err.Error(),
-	}
-}
-
-//NewResponse -
-func NewResponse(code int, msg string) *Response {
-	return &Response{
-		Code: code,
-		Data: msg,
-	}
-}
-
 //logonFromToken - get logon user from token
-func logonFromToken(c echo.Context) *cx.Payload {
+func (s *Service) logonFromToken(c echo.Context) *cx.Payload {
 	user := c.Get("user").(*jwt.Token)
 	claims := user.Claims.(jwt.MapClaims)
 
 	secret := claims["uuid"].(string)
-	payload := security.DecryptString(secret)
+	//payload := security.DecryptString(secret)
+	payload := secret
 
 	var d cx.Payload
 	err := utils.JSON.NewDecoder(strings.NewReader(payload)).Decode(&d)
 	if err != nil {
 		return nil
 	}
+
+	//set tenant id
+	err = s.reacquireTenant(&d)
+	if err != nil {
+	}
+
 	return &d
+}
+
+func (s *Service) reacquireTenant(logon *cx.Payload) error {
+	var id int64
+
+	key := "ACCN_TENANT_" + logon.Email
+	var t struct {
+		TenantID int64 `json:"tenant_id"` //テナントID
+		UserID   int64 `json:"user_id"`   //ユーザーID
+		Right    int   `json:"right"`     //権限
+	}
+	err := s.CacheGet(key, &t)
+	if err != nil {
+		t, err := s.DB().GetUserTenant(nil, logon.ID)
+		if err != nil {
+			return err
+		}
+		id = t.TenantID
+		err = s.CacheSet(key, t)
+		if err != nil {
+
+		}
+	} else {
+		id = t.TenantID
+	}
+	//set tenant id
+	logon.Tenant = id
+
+	return nil
 }
